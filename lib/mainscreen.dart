d9e8804e5345843495786c7f6bc59936b7a33c9b
Sai vishnu <saivishnu725@gmail.com>
2020-12-11 20:11:17 +0530

    Removed the body of Scaffold.

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'data.dart';
+
+var addColor = Colors.white;
+var remColor = Colors.white;
 
 class MainScreen extends StatefulWidget {
   MainScreen({Key key}) : super(key: key);
@@ -18,9 +22,11 @@ class _MainScreenState extends State<MainScreen> {
         title: RaisedButton(
           color: Colors.blueGrey[700],
           onPressed: () {
-            debugPrint("Screen for other weeks list..."); //TODO: Add new screen for displaying all previous weeks list .
+            debugPrint(
+                "Screen for other weeks list..."); //TODO: Add new screen for displaying all previous weeks list .
           },
           elevation: 0.0,
+          splashColor: Colors.blueGrey[700],
           child: Padding(
             padding: const EdgeInsets.only(left: 18.0),
             child: Text(
@@ -35,30 +41,18 @@ class _MainScreenState extends State<MainScreen> {
         ),
         // centerTitle: true,
         leading: Icon(Icons.money_off),
-        actions: [
-          RaisedButton(
-            elevation: 0.0,
-            color: Colors.blueGrey[700],
-            splashColor: Colors.amber[600],
-            child: Text(
-              "Total",
-              style: TextStyle(
-                  // color: Colors.amber[600],
-                  fontWeight: FontWeight.bold),
-            ),
-            onPressed: () {
-              debugPrint("Total of all ppl."); //TODO: Add total salary of all ppl together for the current week.
-            },
-          ),
-          RaisedButton(
-            splashColor: Colors.amber[600],
-            color: Colors.blueGrey[700],
-            elevation: 0.0,
-            child: Icon(Icons.add),
-            onPressed: () {
-              debugPrint("New person add screen."); //TODO: Add a new user screen.
-            },
-          ),
+        actions: [actionAppBar()],
+      ),
+      body: ListView(
+        children: [
+          //Moving away from the template cuz i dont have to add new ppl.
+          ListTile(),//1
+          ListTile(),//2
+          ListTile(),//3
+          ListTile(),//4
+          ListTile(),//5
+          ListTile(),//6
+          ListTile(),//7
         ],
       ),
     );
