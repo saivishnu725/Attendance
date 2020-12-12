ec4f7a26b70a764530c573df462f8b0335449b06
Sai vishnu <saivishnu725@gmail.com>
2020-12-12 19:06:45 +0530

    Gave separate listTile for everyone but somehow got no errors for 6 users but got for 7.

--- lib/data.dart
+++ lib/data.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-List<String> names = ['1', '2', '3', '4', '5', '6'];
+List<String> names = ['1', '2', '3', '4', '5', '6','7','8'];
 Widget actionAppBar() {
   return Row(children: [
     RaisedButton(
--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -46,13 +46,160 @@ class _MainScreenState extends State<MainScreen> {
       body: ListView(
         children: [
           //Moving away from the template cuz i dont have to add new ppl.
-          ListTile(),//1
-          ListTile(),//2
-          ListTile(),//3
-          ListTile(),//4
-          ListTile(),//5
-          ListTile(),//6
-          ListTile(),//7
+          ListTile(
+            leading: Text(
+              "tot_for_0",
+              style: TextStyle(
+                  color: Colors.amber[700], fontWeight: FontWeight.bold),
+            ),
+            title: Text(
+              names[0],
+              style: TextStyle(
+                  color: Colors.yellow[700], fontWeight: FontWeight.bold),
+            ),
+            trailing: RaisedButton.icon(
+              onPressed: () {
+                debugPrint("Add one for 0");
+              },
+              icon: Icon(Icons.add),
+              label: Text(" "),
+              elevation: 0.0,
+              color: Colors.grey[800],
+              splashColor: Colors.yellow[700],
+            ),
+          ), //0
+          ListTile(
+            leading: Text(
+              "tot_for_1",
+              style: TextStyle(
+                  color: Colors.amber[700], fontWeight: FontWeight.bold),
+            ),
+            title: Text(
+              names[1],
+              style: TextStyle(
+                  color: Colors.yellow[700], fontWeight: FontWeight.bold),
+            ),
+            trailing: RaisedButton.icon(
+              onPressed: () {
+                debugPrint("Add one for 1");
+              },
+              icon: Icon(Icons.add),
+              label: Text(" "),
+              elevation: 0.0,
+              color: Colors.grey[800],
+              splashColor: Colors.yellow[700],
+            ),
+          ), //1
+          ListTile(
+            leading: Text(
+              "tot_for_2",
+              style: TextStyle(
+                  color: Colors.amber[700], fontWeight: FontWeight.bold),
+            ),
+            title: Text(
+              names[2],
+              style: TextStyle(
+                  color: Colors.yellow[700], fontWeight: FontWeight.bold),
+            ),
+            trailing: RaisedButton.icon(
+              onPressed: () {
+                debugPrint("Add one for 2");
+              },
+              icon: Icon(Icons.add),
+              label: Text(" "),
+              elevation: 0.0,
+              color: Colors.grey[800],
+              splashColor: Colors.yellow[700],
+            ),
+          ), //2
+          ListTile(
+            leading: Text(
+              "tot_for_3",
+              style: TextStyle(
+                  color: Colors.amber[700], fontWeight: FontWeight.bold),
+            ),
+            title: Text(
+              names[3],
+              style: TextStyle(
+                  color: Colors.yellow[700], fontWeight: FontWeight.bold),
+            ),
+            trailing: RaisedButton.icon(
+              onPressed: () {
+                debugPrint("Add one for 3");
+              },
+              icon: Icon(Icons.add),
+              label: Text(" "),
+              elevation: 0.0,
+              color: Colors.grey[800],
+              splashColor: Colors.yellow[700],
+            ),
+          ), //3
+          ListTile(
+            leading: Text(
+              "tot_for_4",
+              style: TextStyle(
+                  color: Colors.amber[700], fontWeight: FontWeight.bold),
+            ),
+            title: Text(
+              names[4],
+              style: TextStyle(
+                  color: Colors.yellow[700], fontWeight: FontWeight.bold),
+            ),
+            trailing: RaisedButton.icon(
+              onPressed: () {
+                debugPrint("Add one for 4");
+              },
+              icon: Icon(Icons.add),
+              label: Text(" "),
+              elevation: 0.0,
+              color: Colors.grey[800],
+              splashColor: Colors.yellow[700],
+            ),
+          ), //4
+          ListTile(
+            leading: Text(
+              "tot_for_5",
+              style: TextStyle(
+                  color: Colors.amber[700], fontWeight: FontWeight.bold),
+            ),
+            title: Text(
+              names[5],
+              style: TextStyle(
+                  color: Colors.yellow[700], fontWeight: FontWeight.bold),
+            ),
+            trailing: RaisedButton.icon(
+              onPressed: () {
+                debugPrint("Add one for 5");
+              },
+              icon: Icon(Icons.add),
+              label: Text(" "),
+              elevation: 0.0,
+              color: Colors.grey[800],
+              splashColor: Colors.yellow[700],
+            ),
+          ), //5
+          // ListTile(
+          //   leading: Text(
+          //     "tot_for_6",
+          //     style: TextStyle(
+          //         color: Colors.amber[700], fontWeight: FontWeight.bold),
+          //   ),
+          //   title: Text(
+          //     names[6],
+          //     style: TextStyle(
+          //         color: Colors.yellow[700], fontWeight: FontWeight.bold),
+          //   ),
+          //   trailing: RaisedButton.icon(
+          //     onPressed: () {
+          //       debugPrint("Add one for 6");
+          //     },
+          //     icon: Icon(Icons.add),
+          //     label: Text(" "),
+          //     elevation: 0.0,
+          //     color: Colors.grey[800],
+          //     splashColor: Colors.yellow[700],
+          //   ),
+          // ), //6
         ],
       ),
     );
